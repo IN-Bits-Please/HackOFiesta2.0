f6d98b23fc08e2d5d69cbfcb6593751f69d259b7
Shivam Singh <stdnt.shivam@gmail.com>
2021-04-16 23:55:23 +0530

    Sign Up NGO Ready

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.login),
+    path('signUp', views.signUp),
+    path('signUpInd', views.signUpInd),
+    path('signUpNgo', views.signUpNgo)
 ]
--- login/views.py
+++ login/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import *
 
 
 def login(request):
@@ -14,4 +17,26 @@ def signUpInd(request):
 
 
 def signUpNgo(request):
-    return render(request, 'base.html')
+    if request.method == "POST":
+        first_name = request.POST['name']
+        last_name = "NGO"
+        email = request.POST['email']
+        tagline = request.POST['tagline']
+        password = request.POST['password']
+        password_c = request.POST['password_repeat']
+        if password == password_c:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "User already exists.")
+                return redirect('/signUpNgo')
+            user = User.objects.create_user(
+                email=email, password=password, first_name=first_name, last_name=last_name, username=email)
+            user.save()
+            ngo_obj = Ngo(username=email, tagline=tagline)
+            ngo_obj.save()
+            messages.info(request, 'User created succesfully.')
+            return redirect('/signUpNgo')
+
+        else:
+            messages.info(request, 'Passwords don\'t match')
+            return redirect('/signUpNgo')
+    return render(request, 'login/signUpNgo.html')
